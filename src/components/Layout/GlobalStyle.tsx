ac7be34ce915ebe04e22a70c47b846c5e674b747
alfredotang <asdzxcca0617@gmail.com>
2020-11-11 01:12:04 +0800

    調整 browser scrollbar 樣式

--- src/components/Layout/GlobalStyle.tsx
+++ src/components/Layout/GlobalStyle.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
+import { Theme } from '@theme';
 const GlobalStyle: React.FC = () => {
     return (
         <Global
-            styles={css`
+            styles={(theme: Theme) => css`
                 body {
                     overflow-x: hidden;
                     -webkit-overflow-scrolling: touch;
+                    width: 100vw;
                     a {
                         text-decoration: none;
                         &:visited,
@@ -16,6 +18,21 @@ const GlobalStyle: React.FC = () => {
                         }
                     }
                 }
+                &::-webkit-scrollbar {
+                    display: none;
+                }
+
+                /* &::-webkit-scrollbar-track {
+                    background-color: ${theme.palette.background.default};
+                }
+
+                &::-webkit-scrollbar-thumb {
+                    border-radius: 10px;
+                    background-color: ${theme.palette.primary.main};
+                    &:hover {
+                        background-color: ${theme.palette.primary.light};
+                    }
+                } */
             `}
         />
     );
