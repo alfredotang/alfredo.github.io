fccdee6c95bac4ba2be3e66d487184638efd0be2
alfredotang <asdzxcca0617@gmail.com>
2020-11-13 13:23:14 +0800

    加入 category

---
+++ src/templates/CategoryTemplate/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { graphql, Link } from 'gatsby';
+import { Layout } from '@components';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+type IProps = {
+    data: Typing.Query;
+};
+const CategoryTemplate: React.FC<IProps> = ({ data }) => {
+    return (
+        <Layout>
+            <Typography variant="h4">{data.allMdx.edges[0].node.frontmatter.category}</Typography>
+            {data.allMdx.edges.map(({ node }) => (
+                <div key={node.id}>
+                    <Link to={`${node.fields.slug}/`}>
+                        <Button variant="contained">{node.frontmatter.title}</Button>
+                    </Link>
+                </div>
+            ))}
+        </Layout>
+    );
+};
+export default CategoryTemplate;
+export const pageQuery = graphql`
+    query($category: String!) {
+        allMdx(filter: { frontmatter: { published: { eq: true }, category: { eq: $category } } }) {
+            edges {
+                node {
+                    id
+                    excerpt
+                    frontmatter {
+                        date(formatString: "YYYY/MM/DD")
+                        title
+                        tag
+                        category
+                    }
+                    fields {
+                        slug
+                    }
+                }
+            }
+        }
+    }
+`;
