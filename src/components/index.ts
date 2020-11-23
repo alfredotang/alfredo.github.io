5dd6e44a54922398a330e70aaa6452a675bc018a
alfredotang <asdzxcca0617@gmail.com>
2020-11-23 11:32:32 +0800

    加入 TOC & Tree

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,7 @@ import { default as LimitLine } from './LimitLine';
 import { default as Tag } from './Tag';
 import { ArticleList, ArticleMaker, ArticleTitle } from './Article';
 import { default as MDXComponents } from './MDXComponents';
+import { default as TOC } from './TOC';
+import { default as Tree } from './Tree';
 
-export { Layout, LimitLine, ArticleList, ArticleTitle, ArticleMaker, Tag, MDXComponents };
+export { Layout, LimitLine, ArticleList, ArticleTitle, ArticleMaker, Tag, MDXComponents, TOC, Tree };
